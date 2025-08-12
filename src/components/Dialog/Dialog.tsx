7cb06164991b6ab5979c460c89d97623f675662a
Jon Mcgugan <j.mcgugan@outlook.com>
2025-08-12 10:46:28 -0400

    removed icons package, it was causing conflicts.

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,5 +1,4 @@
-import { IconButton} from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
+import { IconButton, Icon } from '@mui/material';
 
 import './Dialog.scss'
 
@@ -16,7 +15,7 @@ export default function Dialog({ title, description, actions }: DialogProps) {
                     {title}
                 </h4>
                 <IconButton className='dialog__header-close-button'>
-                    <CloseIcon/>
+                    <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiSvgIcon-root MuiSvgIcon-fontSizeMedium svg-icon css-5zsjn4" focusable="false" aria-hidden="true" viewBox="0 0 24 24" height="24px" width="24px" tabindex="-1" title="Close"><path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
                 </IconButton>
             </header>
             {description && (
